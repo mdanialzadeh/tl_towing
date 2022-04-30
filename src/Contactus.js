3bb3ec914d8c1b48ee2a256a5928313b34d22644
Mdanialzadeh <mdanialzadeh10@gmail.com>
2022-04-30 16:15:31 -0400

    commit

--- src/Contactus.js
+++ src/Contactus.js
@@ -28,38 +28,88 @@ function Contactus() {
   };
 
   return (
-    <Container style={{ paddingTop: "4em" }}>
-      <Row sm={1} md={2}>
-        <Col sm="true">
-          <div style={{ color: "WHITE" }}>
-            BEFORE CONTACTING US Send us your information by filling out the
-            short contact form and we will get in touch with you right away or
-            you can contact us at (347) 695-8447 CONTACT INFORMATION 14367 226th
-            Street Jamaica, NY 11413 (347) 695-8447 24/7 Availability
-          </div>
-        </Col>
-        <Col>
-          <div>
-            <form
-              ref={form}
-              onSubmit={sendEmail}
+    <Container
+      fluid
+      style={{
+        background: "rgb(2,0,36)",
+        background:
+          "linear-gradient(180deg, rgba(0,0,0,.85) 0%, rgba(0,0,0,.9) 100%)",
+        paddingBottom: "4em",
+      }}
+    >
+      <div
+        style={{ padding: "2em 0 1em 0", fontSize: "1.62rem" }}
+        className="divider"
+      >
+        Contact Us
+      </div>
+      <Container style={{ display: "flex !important", alignItems: "center" }}>
+        <Row md={1} lg={2} className="justify-content-center">
+          <Col sm="true" lg={3}>
+            <div
               style={{
+                color: "white",
+                height: "100%",
                 display: "flex",
-                flexDirection: "column",
-                textAllign: "center",
+                alignItems: "center",
               }}
             >
-              <label>Name</label>
-              <input type="text" name="user_name" placeholder="Full Name" />
-              <label>Email</label>
-              <input type="email" name="user_email" placeholder="Email" />
-              <label>Message</label>
-              <textarea name="message" />
-              <input type="submit" value="Send" />
-            </form>
-          </div>
-        </Col>
-      </Row>
+              <p>
+                Questions? Comments? Concerns? <br />
+                Please fill out the form and someone will get back to you as
+                soon as possible. or Email us directly at{" "}
+                <a
+                  style={{ color: "white", textDecoration: "underline" }}
+                  href="mailto:tltowingny@gmail.com"
+                >
+                  Tltowingny@gmail.com
+                </a>
+              </p>
+            </div>
+          </Col>
+          <Col sm="true" lg={6}>
+            <div>
+              <form
+                ref={form}
+                onSubmit={sendEmail}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  textAllign: "center",
+                }}
+              >
+                <input
+                  className="txtinput"
+                  type="text"
+                  name="user_name"
+                  placeholder="Full Name"
+                />
+
+                <input
+                  className="txtinput"
+                  type="email"
+                  name="user_email"
+                  placeholder="Email"
+                />
+                <input
+                  className="txtinput"
+                  type="phone"
+                  name="user_phone"
+                  placeholder="Email"
+                />
+
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  className="txtinput"
+                  style={{ minHeight: "200px" }}
+                />
+                <input className="submitbtn" type="submit" value="Send" />
+              </form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
